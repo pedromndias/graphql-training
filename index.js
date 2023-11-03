94a21d4fdc254a152638764ccf2528f909547158
Pedro Dias <pedromndias@gmail.com>
2023-11-03 20:34:23 +0100

    add resolvers and mutations. basic queries done

--- index.js
+++ index.js
@@ -9,8 +9,14 @@ const resolvers = {
         games() {
             return db.games
         },
+        game(_, args) {
+            return db.games.find((game) => game.id === args.id)
+        },
         authors() {
             return db.authors
+        },
+        author(_, args) {
+            return db.authors.find((author) => author.id === args.id)
         },
         reviews() {
             return db.reviews
@@ -18,6 +24,54 @@ const resolvers = {
         // The next resolver will be to query for a specific review by its id. It can take 3 arguments: parent, args and context (for the moment we will ignore the first and last)
         review(_, args) {
             return db.reviews.find((review) => review.id === args.id) // This returns a specific review by its id.
+        }
+    },
+    // Let's create a resolver for Game, so we can get the reviews of a specific game:
+    Game: {
+        reviews(parent) { // The parent parameter is a reference of the value return by the parent resolver (game)
+            return db.reviews.filter((r) => r.game_id === parent.id)
+        }
+    },
+    // And now the same for the authors, let's get all the reviews by a specific author:
+    Author: {
+        reviews(parent) {
+            return db.reviews.filter((r) => r.author_id === parent.id)
+        }
+    },
+    Review: {
+        // Find the author of a specific review:
+        author(parent) {
+            return db.authors.find((a) => a.id === parent.author_id)
+        },
+        // Find the game of a specific review:
+        game(parent) {
+            return db.games.find((g) => g.id === parent.game_id)
+        }
+    },
+    Mutation: {
+        deleteGame(_, args) {
+            db.games = db.games.filter((g) => g.id !== args.id)
+
+            return db.games
+        },
+        addGame(_, args) {
+            let game = {
+                ...args.game,
+                id: Math.floor(Math.random() * 10000).toString() // Create a new random id.
+            }
+            db.games.push(game)
+            return game
+        },
+        updateGame(_, args) {
+            db.games = db.games.map((g) => {
+                if (g.id === args.id) {
+                    return {...g, ...args.edits} // Note how if we update the title (for example), that new title from "...args.edits" will overwrite the one from "...g".
+                }
+
+                return g
+            })
+
+            return db.games.find((g) => g.id === args.id)
         }
     }
 }
--- schema.js
+++ schema.js
@@ -3,23 +3,47 @@ export const typeDefs = `#graphql
         id: ID! #note the "!" for required
         title: String!
         platform: [String!]!
+        reviews: [Review!]
     }
     type Review {
         id: ID!
         rating: Int!
         content: String!
+        game: Game! #relation between a review and a game
+        author: Author! #relation between an author and a game
     }
     type Author {
         id: ID!
         name: String!
         verified: Boolean!
+        reviews: [Review!]
     }
     #all GraphQL schema needs a Query type:
     type Query {
         reviews: [Review]
         review(id: ID!): Review #This is to query a single review by id.
         games: [Game]
+        game(id: ID!): Game
         authors: [Author]
+        author(id: ID!): Author
+    }
+    #Now let's determine how users can mutate the data:
+    type Mutation {
+        #To create a new game we need to pass all the properties as arguments (besides the id). In this case we will use an input called AddGameInput (created down bellow):
+        addGame(game: AddGameInput): Game
+        
+        deleteGame(id: ID!): [Game] #When a user deletes a game, we return the updated array of games.
+
+        updateGame(id: ID!, edits: EditGameInput!): Game
+    }
+    #Let's create a collection of fields that can be used as a single argument:
+    input AddGameInput {
+        title: String!
+        platform: [String!]!
+    }
+    input EditGameInput {
+        title: String
+        platform: [String!]
     }
 `
 
